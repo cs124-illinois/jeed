8e2cecb8b85fec11eef472c562ec17fd7ce37f5a
Geoffrey Challen <challen@illinois.edu>
2023-06-05 14:17:40 -0500

    Bump version for fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs124-illinois.jeed"
-    version = "2023.6.1"
+    version = "2023.6.2"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
