7bdd40cdeee43d72771559b010cadef9aa8a48e2
Geoffrey Challen <challen@illinois.edu>
2024-02-11 07:56:28 -0600

    Latest.

--- js/demo/next.config.js
+++ js/demo/next.config.js
@@ -2,4 +2,6 @@ module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
   ...(process.env.NEXT_PUBLIC_BASE_PATH && { assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH }),
   reactStrictMode: true,
+  output: "export",
+  distDir: "dist",
 }
