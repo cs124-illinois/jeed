633d4c9bb62358ed0a0e0d4b52bee206fb541d4f
Geoffrey Challen <challen@illinois.edu>
2025-03-12 15:12:54 -0500

    Version fix.

--- core/src/main/kotlin/Version.kt
+++ core/src/main/kotlin/Version.kt
@@ -2,12 +2,6 @@ package edu.illinois.cs.cs125.jeed.core
 
 import java.util.Properties
 
-val VERSION = run {
-    @Suppress("TooGenericExceptionCaught")
-    try {
-        val versionFile = object {}::class.java.getResource("/edu.illinois.cs.cs125.jeed.version")
-        Properties().also { it.load(versionFile!!.openStream()) }["version"] as String
-    } catch (_: Exception) {
-        "unspecified"
-    }
-}
+val VERSION: String = Properties().also {
+    it.load((object {}).javaClass.getResourceAsStream("/edu.illinois.cs.cs125.jeed.version"))
+}.getProperty("version")
