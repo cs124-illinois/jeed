881b7ef17900eaba26219327c94150c5bfe67336
Geoffrey Challen <challen@illinois.edu>
2025-03-12 14:42:49 -0500

    Small version fix.

--- core/src/main/kotlin/Version.kt
+++ core/src/main/kotlin/Version.kt
@@ -6,9 +6,8 @@ val VERSION = run {
     @Suppress("TooGenericExceptionCaught")
     try {
         val versionFile = object {}::class.java.getResource("/edu.illinois.cs.cs125.jeed.version")
-        Properties().also { it.load(versionFile.openStream()) }["version"] as String
-    } catch (e: Exception) {
-        println(e)
+        Properties().also { it.load(versionFile!!.openStream()) }["version"] as String
+    } catch (_: Exception) {
         "unspecified"
     }
 }
