7607bc7c135dadf6f2f4722e823762114abac2a5
Geoffrey Challen <challen@illinois.edu>
2025-03-20 12:38:27 -0500

    Memory changes.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -77,6 +77,7 @@ tasks.register<Exec>("dockerPush") {
     workingDir(layout.buildDirectory.dir("docker"))
     commandLine(
         ("/usr/local/bin/docker buildx build . --platform=linux/amd64,linux/arm64/v8 " +
+            "--builder multiplatform " +
             "--tag ${dockerName}:latest " +
             "--tag ${dockerName}:${project.version} --push").split(" ")
     )
